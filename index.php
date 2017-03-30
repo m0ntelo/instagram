8c21e85f3a258e17ce76ff49ff515f6aaf5ac1a4
Wilson Montelo <wilsonmontelo0@gmail.com>
2017-03-29 22:57:37 -0300

    Primeiro Commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt">
+  <head>
+    <title>Instagram</title>
+    <meta charset="utf-8">
+    <meta http-equiv="Content-Language" content="pt-br">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="description" content="Logo feita em HTML e CSS">
+    <meta name="keywords" content="HTML, CSS, Front End, Instagram">
+    <meta name="author" content="Wilson Montelo">
+    <!-- Estilos-->
+    <link rel="stylesheet" href="css/estilos.css">
+  </head>
+<body>
+<section id="content">
+	<div id="gradient">
+		<div id="borda-interna">
+			<div id="circulo-topo"></div>
+			<div id="circulo-centro"></div>
+		</div>
+	</div>
+</section>
+</body>
+</html>
